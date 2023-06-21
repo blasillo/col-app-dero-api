cc871031ad256f966265aed4ff00638ea23a87ca
blas <btorregrosa@infor.uva.es>
2023-06-21 12:37:43 +0200

    Inicio proyecto

---
+++ src/main/java/es/jcyl/eclap/api/colapp/ColAppDeroApiApplication.java
@@ -0,0 +1,13 @@
+package es.jcyl.eclap.api.colapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColAppDeroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColAppDeroApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/jcyl/eclap/api/colapp/ColAppDeroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.jcyl.eclap.api.colapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColAppDeroApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
