6356bd03e3f92194719b6263e1065697d5df99aa
blas <btorregrosa@infor.uva.es>
2023-06-27 13:43:19 +0200

    swagger docs

--- src/main/java/es/jcyl/eclap/api/colapp/ColAppDeroApiApplication.java
+++ src/main/java/es/jcyl/eclap/api/colapp/ColAppDeroApiApplication.java
@@ -3,10 +3,9 @@ package es.jcyl.eclap.api.colapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-@EnableWebMvc
 public class ColAppDeroApiApplication {
 
     public static void main(String[] args) {
